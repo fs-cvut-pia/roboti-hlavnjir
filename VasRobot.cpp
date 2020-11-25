b7bc78cc9e2a1ec3247a84cdb945fdf11f80337b
hlavnjir <71826585+hlavnjir@users.noreply.github.com>
2020-11-25 22:13:11 +0100

    Změna - NEFUNGUJE

--- VasRobot.cpp
+++ VasRobot.cpp
@@ -1,17 +1,43 @@
 #include "VasRobot.h"
+#include <cstdlib>
 
-bool VasRobot::start() {
+//bool VasRobot::start(Bludiste & map) {smer = 0; return true; }
+//bool VasRobot::start(BludisteOdkryte & map) { return true; }
+//bool VasRobot::stop() {return true; }
 
+void VasRobot::krok(Bludiste & map){
+    smer = rand() % 4;
+
+    if (smer == 0) {
+        map.dolu();
+    }
+    else if (smer == 1) {
+        map.vlevo();
+    }
+    else if (smer == 2) {
+        map.nahoru();
+    }
+    else if (smer == 3) {
+        map.vpravo();
+    }
 }
 
-bool VasRobot::stop() {
+void VasRobot::krok(BludisteOdkryte &map) {
+    smer = rand() % 4;
 
+    if (smer == 0) {
+        map.dolu();
+    }
+    else if (smer == 1) {
+        map.vlevo();
+    }
+    else if (smer == 2) {
+        map.nahoru();
+    }
+    else if (smer == 3) {
+        map.vpravo();
+    }
 }
 
-bool VasRobot::krok() {
 
-}
-
-std::string VasRobot::jmeno() {
-
-}
+//std::string VasRobot::jmeno () { return "Nahodny robot"; }
--- VasRobot.h
+++ VasRobot.h
@@ -1,8 +1,21 @@
-#include <vector>
-#include <string>
+#ifndef VASROBOT_H
+#define VASROBOT_H
+
+#include <iostream>
+#include "Prohledavac.h"
 
 class VasRobot : public Prohledavac {
 public:
-
+    VasRobot() { srand(time(nullptr)); }
+    virtual bool start(Bludiste & map) override {return true;};
+    virtual bool start(BludisteOdkryte & map) override {return true;};
+    virtual bool stop() override { return false; };
+    virtual void krok(Bludiste & map) override;
+    virtual void krok(BludisteOdkryte & map);
+    virtual std::string jmeno();
 private:
+    int smer{};
 };
+
+#endif
+
